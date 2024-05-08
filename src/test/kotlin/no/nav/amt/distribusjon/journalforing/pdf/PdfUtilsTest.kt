b56137ba90a5212e7898d236b3ea33a55c82a479
tiafiring <tiafiring@gmail.com>
2024-05-08 15:03:52 +0200

    viser innhold som string, ikke json i pdf (#35)

--- src/test/kotlin/no/nav/amt/distribusjon/journalforing/pdf/PdfUtilsTest.kt
+++ src/test/kotlin/no/nav/amt/distribusjon/journalforing/pdf/PdfUtilsTest.kt
@@ -1,7 +1,9 @@
 package no.nav.amt.distribusjon.journalforing.pdf
 
 import io.kotest.matchers.shouldBe
+import no.nav.amt.distribusjon.hendelse.model.Aarsak
 import no.nav.amt.distribusjon.hendelse.model.Hendelse
+import no.nav.amt.distribusjon.hendelse.model.Innhold
 import no.nav.amt.distribusjon.utils.data.HendelseTypeData
 import no.nav.amt.distribusjon.utils.data.Hendelsesdata
 import no.nav.amt.distribusjon.utils.data.Persondata
@@ -34,5 +36,50 @@ class PdfUtilsTest {
 
         pdfDto.endringer.size shouldBe 1
         (pdfDto.endringer.first() as EndringDto.ForlengDeltakelse).sluttdato shouldBe LocalDate.now().plusWeeks(4)
+    }
+
+    @Test
+    fun `lagEndringsvedtakPdfDto - IkkeAktuell - inneholder arsak som string`() {
+        val deltaker = Hendelsesdata.deltaker()
+        val navBruker = Persondata.lagNavBruker()
+        val ansvarligNavVeileder = Hendelsesdata.ansvarligNavVeileder()
+        val arsak = Aarsak(Aarsak.Type.IKKE_MOTT)
+        val hendelser: List<Hendelse> = listOf(
+            Hendelsesdata.hendelse(
+                HendelseTypeData.ikkeAktuell(arsak),
+                deltaker = deltaker,
+                ansvarlig = ansvarligNavVeileder,
+                opprettet = LocalDateTime.now().minusMinutes(20),
+            ),
+        )
+
+        val pdfDto = lagEndringsvedtakPdfDto(deltaker, navBruker, ansvarligNavVeileder, hendelser)
+
+        pdfDto.endringer.size shouldBe 1
+        (pdfDto.endringer.first() as EndringDto.IkkeAktuell).aarsak shouldBe arsak.visningsnavn()
+    }
+
+    @Test
+    fun `lagEndringsvedtakPdfDto - EndreInnhold - inneholder innhold som string`() {
+        val deltaker = Hendelsesdata.deltaker()
+        val navBruker = Persondata.lagNavBruker()
+        val ansvarligNavVeileder = Hendelsesdata.ansvarligNavVeileder()
+        val innhold = listOf(
+            Innhold("tekst 1", "kode 1", null),
+            Innhold("tekst 2", "kode 2", null),
+        )
+        val hendelser: List<Hendelse> = listOf(
+            Hendelsesdata.hendelse(
+                HendelseTypeData.endreInnhold(innhold),
+                deltaker = deltaker,
+                ansvarlig = ansvarligNavVeileder,
+                opprettet = LocalDateTime.now().minusMinutes(20),
+            ),
+        )
+
+        val pdfDto = lagEndringsvedtakPdfDto(deltaker, navBruker, ansvarligNavVeileder, hendelser)
+
+        pdfDto.endringer.size shouldBe 1
+        (pdfDto.endringer.first() as EndringDto.EndreInnhold).innhold shouldBe listOf("tekst 1", "tekst 2")
     }
 }
