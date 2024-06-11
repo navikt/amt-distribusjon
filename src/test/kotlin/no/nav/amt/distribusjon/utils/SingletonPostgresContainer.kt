03f1b49b18d335f5e3d0182839976fb11dd93ba5
Tormod Fossum <tormod.fossum@nav.no>
2024-06-11 10:20:26 +0200

    Drop alle tabeller i cleanup

--- src/test/kotlin/no/nav/amt/distribusjon/utils/SingletonPostgresContainer.kt
+++ src/test/kotlin/no/nav/amt/distribusjon/utils/SingletonPostgresContainer.kt
@@ -69,13 +69,16 @@ object SingletonPostgresContainer {
             queryOf("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
                 .map { it.string("table_name") }
                 .asList,
-        ).filterNot { table -> table == "flyway_schema_history" }
+        )
 
         it.transaction { tx ->
             tables.forEach { table ->
                 val sql = "truncate table $table cascade"
                 log.info("Truncating table $table...")
                 tx.run(queryOf(sql).asExecute)
+                val drop = "drop table $table"
+                log.info("Dropping table $table...")
+                tx.run(queryOf(drop).asExecute)
             }
         }
     }
