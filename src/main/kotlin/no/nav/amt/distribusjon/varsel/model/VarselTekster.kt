d07f535e00ce248fd33ae8d7eabc1f6ccf2c1d53
Line Moseng <line.moseng@nav.no>
2025-04-09 10:08:21 +0200

    ny tekst på varsel for utkast for felles oppstart

--- src/main/kotlin/no/nav/amt/distribusjon/varsel/model/VarselTekster.kt
+++ src/main/kotlin/no/nav/amt/distribusjon/varsel/model/VarselTekster.kt
@@ -2,20 +2,29 @@ package no.nav.amt.distribusjon.varsel.model
 
 import no.nav.amt.distribusjon.hendelse.model.Hendelse
 import no.nav.amt.distribusjon.journalforing.pdf.visningsnavn
+import no.nav.amt.lib.models.hendelse.HendelseDeltaker
 import no.nav.amt.lib.models.hendelse.HendelseType
 
 const val OPPGAVE_TEKST = "Du har mottatt et utkast til påmelding på arbeidsmarkedstiltaket: %s hos %s. Svar på spørsmålet her."
+const val OPPGAVE_FELLES_OPPSTART_TEKST = "Du har mottatt et utkast til søknad på arbeidsmarkedstiltaket %s hos %s. Svar på spørsmålet her."
 
 const val BESKJED_TEKST = "Ny endring på arbeidsmarkedstiltaket: %s hos %s."
 
 const val MELDT_PA_DIREKTE_TEKST = "Du er meldt på arbeidsmarkedstiltaket: %s hos %s."
 
-fun oppgaveTekst(hendelse: Hendelse) = String.format(
-    OPPGAVE_TEKST,
+fun oppgaveTekst(hendelse: Hendelse): String {
+  val tekst = if (hendelse.deltaker.deltakerliste.oppstartstype == HendelseDeltaker.Deltakerliste.Oppstartstype.FELLES) {
+    OPPGAVE_FELLES_OPPSTART_TEKST
+  } else {
+    OPPGAVE_TEKST
+  }
+  return String.format(
+    tekst,
     hendelse.deltaker.deltakerliste.tiltak.navn,
     hendelse.deltaker.deltakerliste.arrangor
-        .visningsnavn(),
-)
+      .visningsnavn(),
+  )
+}
 
 fun beskjedTekst(hendelse: Hendelse): String {
     val tekst = if (hendelse.payload is HendelseType.NavGodkjennUtkast) {
