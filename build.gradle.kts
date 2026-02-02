5b384a9751059835009866758b4819d4b67cce8b
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-02-03 00:56:09 +0100

    kotlinVersion = "2.2.21"

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ group = "no.nav.amt-distribusjon"
 version = "1.0-SNAPSHOT"
 
 plugins {
-    val kotlinVersion = "2.3.0"
+    val kotlinVersion = "2.2.21"
 
     kotlin("jvm") version kotlinVersion
     id("io.ktor.plugin") version "3.4.0"
@@ -34,6 +34,7 @@ val caffeineVersion = "3.2.3"
 val unleashVersion = "11.2.1"
 val nimbusVersion = "10.7"
 val amtLibVersion = "1.2026.02.01_09.45-d68624f62ecf"
+val junitJupiterVersion = "6.0.2"
 
 // fjernes ved neste release av org.apache.kafka:kafka-clients
 configurations.configureEach {
@@ -94,6 +95,7 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-json-jvm:$kotestVersion")
     testImplementation("com.nimbusds:nimbus-jose-jwt:$nimbusVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
 }
 
 kotlin {
