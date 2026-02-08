4bda7dcea9037b38dfce77400be129a92eba1dbe
Izis Frøyhov <izisfroyhov@gmail.com>
2026-02-08 10:54:55 +0100

    Bruker ny modell for felles innholdskomponent (#466)
    * Bruker ny modell for felles innholdskomponent
    
    * Retter beskjed tekst som vises på nav til å bruke påmeldingstype
    
    * Bruker ny mal som dekker behovet for "kurs", vta og "alltid individuelle" tiltak slik at vi ikke må legge inn håndtering av tiltakstyper i malen

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ val postgresVersion = "42.7.9"
 val caffeineVersion = "3.2.3"
 val unleashVersion = "12.1.0"
 val nimbusVersion = "10.7"
-val amtLibVersion = "1.2026.02.03_19.58-43b3622ee405"
+val amtLibVersion = "1.2026.02.05_14.55-a3671d1ac660"
 val junitJupiterVersion = "6.0.2"
 
 // fjernes ved neste release av org.apache.kafka:kafka-clients
--- src/main/kotlin/no/nav/amt/distribusjon/varsel/model/VarselTekster.kt
+++ src/main/kotlin/no/nav/amt/distribusjon/varsel/model/VarselTekster.kt
@@ -3,14 +3,13 @@ package no.nav.amt.distribusjon.varsel.model
 import no.nav.amt.distribusjon.hendelse.model.Hendelse
 import no.nav.amt.distribusjon.journalforing.pdf.visningsnavn
 import no.nav.amt.lib.models.deltakerliste.GjennomforingPameldingType
-import no.nav.amt.lib.models.hendelse.HendelseDeltaker
 import no.nav.amt.lib.models.hendelse.HendelseType
 
 const val OPPGAVE_TEKST = "Du har mottatt et utkast til påmelding på arbeidsmarkedstiltaket: %s hos %s. Svar på spørsmålet her."
 const val OPPGAVE_TRENGER_GODKJENNING = "Du har mottatt et utkast til søknad på arbeidsmarkedstiltaket %s hos %s. Svar på spørsmålet her."
 const val BESKJED_TEKST = "Ny endring på arbeidsmarkedstiltaket: %s hos %s."
 const val MELDT_PA_DIREKTE_TEKST = "Du er meldt på arbeidsmarkedstiltaket: %s hos %s."
-const val SOKT_INN_FELLES_OPPSTART_TEKST = "Du er søkt inn på arbeidsmarkedstiltaket %s hos %s."
+const val SOKT_INN_TRENGER_GODKJENNING_TEKST = "Du er søkt inn på arbeidsmarkedstiltaket %s hos %s."
 const val FATT_PLASS = "Du har fått plass på arbeidsmarkedstiltaket: %s hos %s"
 
 fun oppgaveTekst(hendelse: Hendelse): String {
@@ -30,10 +29,10 @@ fun oppgaveTekst(hendelse: Hendelse): String {
 fun beskjedTekst(hendelse: Hendelse): String {
     val tekst = when {
         (
-            hendelse.deltaker.deltakerliste.oppstartstype == HendelseDeltaker.Deltakerliste.Oppstartstype.FELLES &&
+            hendelse.deltaker.deltakerliste.pameldingstype == GjennomforingPameldingType.TRENGER_GODKJENNING &&
                 (hendelse.payload is HendelseType.NavGodkjennUtkast || hendelse.payload is HendelseType.ReaktiverDeltakelse)
         )
-        -> SOKT_INN_FELLES_OPPSTART_TEKST
+        -> SOKT_INN_TRENGER_GODKJENNING_TEKST
         hendelse.payload is HendelseType.NavGodkjennUtkast -> MELDT_PA_DIREKTE_TEKST
         hendelse.payload is HendelseType.TildelPlass -> FATT_PLASS
         else -> BESKJED_TEKST
