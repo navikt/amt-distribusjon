f790cc5f0319c8ce75860c7dd797f074ab8c8514
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-02-11 12:43:45 +0100

    Lagt til sjekk på tom innholdsliste i toInnholdPdfDto

--- src/test/kotlin/no/nav/amt/distribusjon/journalforing/pdf/PdfUtilsTest.kt
+++ src/test/kotlin/no/nav/amt/distribusjon/journalforing/pdf/PdfUtilsTest.kt
@@ -1,5 +1,9 @@
 package no.nav.amt.distribusjon.journalforing.pdf
 
+import io.kotest.assertions.assertSoftly
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import no.nav.amt.distribusjon.hendelse.model.Hendelse
 import no.nav.amt.distribusjon.hendelse.model.visningsnavn
@@ -8,14 +12,68 @@ import no.nav.amt.distribusjon.utils.data.Hendelsesdata
 import no.nav.amt.distribusjon.utils.data.Persondata
 import no.nav.amt.distribusjon.utils.formatDateWithMonthName
 import no.nav.amt.lib.models.deltaker.DeltakerEndring
+import no.nav.amt.lib.models.deltaker.Innhold
 import no.nav.amt.lib.models.deltakerliste.tiltakstype.Tiltakskode
 import no.nav.amt.lib.models.hendelse.InnholdDto
 import no.nav.amt.lib.models.journalforing.pdf.EndringDto
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import java.time.LocalDateTime
 
 class PdfUtilsTest {
+    @Nested
+    inner class ToInnholdPdfDtoTests {
+        @Test
+        fun `innhold med innholdskode == annet`() {
+            val innholdListeInTest = listOf(
+                Innhold(
+                    tekst = "tekst 1",
+                    innholdskode = "annet",
+                    valgt = true,
+                    beskrivelse = "~innholdsbeskrivelse~",
+                ),
+            )
+
+            val innholdPdfDto = innholdListeInTest.toInnholdPdfDto("~ledetekst~")
+
+            assertSoftly(innholdPdfDto.shouldNotBeNull()) {
+                valgteInnholdselementer.shouldBeEmpty()
+                fritekstBeskrivelse shouldBe "~innholdsbeskrivelse~"
+                ledetekst shouldBe "~ledetekst~"
+            }
+        }
+
+        @Test
+        fun `innhold med innholdskode != annet`() {
+            val innholdListeInTest = listOf(
+                Innhold(
+                    tekst = "tekst 1",
+                    innholdskode = "~innholdskode~",
+                    valgt = true,
+                    beskrivelse = "~innholdsbeskrivelse~",
+                ),
+            )
+
+            val innholdPdfDto = innholdListeInTest.toInnholdPdfDto("~ledetekst~")
+
+            assertSoftly(innholdPdfDto.shouldNotBeNull()) {
+                valgteInnholdselementer shouldBe listOf("tekst 1: ~innholdsbeskrivelse~")
+                fritekstBeskrivelse shouldBe null
+                ledetekst shouldBe "~ledetekst~"
+            }
+        }
+
+        @Test
+        fun `tomt innhold`() {
+            val innholdListeInTest = emptyList<Innhold>()
+
+            val innholdPdfDto = innholdListeInTest.toInnholdPdfDto(null)
+
+            innholdPdfDto.shouldBeNull()
+        }
+    }
+
     @Test
     fun `lagEndringsvedtakPdfDto - to endringer av samme type - bruker nyeste endring`() {
         val deltaker = Hendelsesdata.lagDeltaker()
