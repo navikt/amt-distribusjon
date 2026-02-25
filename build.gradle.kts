de70b462d3bcc223daa407586e2d29c8af0846bf
Roar Skinderviken <7499642+roar-skinderviken@users.noreply.github.com>
2026-02-25 18:42:17 +0100

    Deps til libs.versions.toml (#479)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,37 +2,16 @@ group = "no.nav.amt-distribusjon"
 version = "1.0-SNAPSHOT"
 
 plugins {
-    val kotlinVersion = "2.2.21"
-
-    kotlin("jvm") version kotlinVersion
-    id("io.ktor.plugin") version "3.4.0"
-    id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion
-    id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.ktlint)
     application
-    distribution
 }
 
 repositories {
     mavenCentral()
     maven { setUrl("https://github-package-registry-mirror.gc.nav.no/cached/maven-release") }
 }
-
-val ktorVersion = "3.4.0"
-val logbackVersion = "1.5.32"
-val prometeusVersion = "1.16.3"
-val ktlintVersion = "1.6.0"
-val jacksonVersion = "2.21.0"
-val logstashEncoderVersion = "9.0"
-val commonVersion = "3.2025.10.10_08.21-bb7c7830d93c"
-val kotestVersion = "6.1.3"
-val flywayVersion = "12.0.1"
-val hikariVersion = "7.0.2"
-val kotliqueryVersion = "1.9.1"
-val postgresVersion = "42.7.10"
-val caffeineVersion = "3.2.3"
-val unleashVersion = "12.1.1"
-val nimbusVersion = "10.7"
-val amtLibVersion = "1.2026.02.23_11.32-2482c3f13c63"
 
 // fjernes ved neste release av org.apache.kafka:kafka-clients
 configurations.configureEach {
@@ -46,53 +25,48 @@ configurations.configureEach {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-    implementation("io.ktor:ktor-serialization-jackson-jvm")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
 
-    implementation("io.ktor:ktor-server-call-logging-jvm")
-    implementation("io.ktor:ktor-server-call-id-jvm")
-    implementation("io.ktor:ktor-server-metrics-micrometer-jvm")
-    implementation("io.micrometer:micrometer-registry-prometheus:$prometeusVersion")
-    implementation("io.ktor:ktor-server-host-common-jvm")
-    implementation("io.ktor:ktor-server-status-pages-jvm")
-    implementation("io.ktor:ktor-server-auth:$ktorVersion")
-    implementation("io.ktor:ktor-server-auth-jwt:$ktorVersion")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("io.ktor:ktor-client-core:$ktorVersion")
-    implementation("io.ktor:ktor-client-cio:$ktorVersion")
-    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    // --- Ktor BOM ---
+    implementation(platform(libs.ktor.bom))
 
-    implementation("com.github.ben-manes.caffeine:caffeine:$caffeineVersion")
+    // --- Ktor ---
+    implementation(libs.bundles.ktor.server)
+    implementation(libs.bundles.ktor.client)
 
-    implementation("no.nav.tms.varsel:kotlin-builder:2.1.1")
+    // --- Serialization ---
+    implementation(libs.jackson.datatype.jsr310)
 
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
-    implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
-    implementation("no.nav.common:log:$commonVersion")
+    // --- Metrics ---
+    implementation(libs.micrometer.prometheus)
 
-    implementation("com.zaxxer:HikariCP:$hikariVersion")
-    implementation("org.flywaydb:flyway-core:$flywayVersion")
-    implementation("org.flywaydb:flyway-database-postgresql:$flywayVersion")
-    implementation("org.postgresql:postgresql:$postgresVersion")
-    implementation("com.github.seratch:kotliquery:$kotliqueryVersion")
+    // --- Cache ---
+    implementation(libs.caffeine)
 
-    implementation("io.getunleash:unleash-client-java:$unleashVersion")
+    // --- Logging ---
+    implementation(libs.bundles.logging)
 
-    implementation("no.nav.amt.lib:kafka:$amtLibVersion")
-    implementation("no.nav.amt.lib:utils:$amtLibVersion")
-    implementation("no.nav.amt.lib:models:$amtLibVersion")
-    implementation("no.nav.amt.lib:outbox:$amtLibVersion")
+    // --- NAV AMT ---
+    implementation(libs.bundles.amt)
 
-    testImplementation("no.nav.amt.lib:testing:$amtLibVersion")
-    testImplementation("io.ktor:ktor-server-test-host")
-    testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
-    testImplementation("io.kotest:kotest-assertions-json-jvm:$kotestVersion")
-    testImplementation("com.nimbusds:nimbus-jose-jwt:$nimbusVersion")
+    // --- POAO ---
+    implementation(libs.poao.tilgang.client)
+
+    // --- Varsel ---
+    implementation(libs.tms.varsel.kotlin.builder)
+
+    // --- Database ---
+    implementation(libs.bundles.database)
+
+    // --- Feature Toggle ---
+    implementation(libs.unleash)
+
+    // --- Test ---
+    testImplementation(libs.bundles.ktor.test)
+    testImplementation(libs.bundles.kotest)
+    testImplementation(libs.kotlin.test.junit5)
+    testImplementation(libs.mockk)
+    testImplementation(libs.nimbus.jose.jwt)
+    testImplementation(libs.amt.testing)
 }
 
 kotlin {
@@ -110,10 +84,10 @@ application {
 }
 
 ktlint {
-    version = ktlintVersion
+    version = libs.versions.ktlint.cli.version
 }
 
-tasks.test {
+tasks.named<Test>("test") {
     useJUnitPlatform()
     jvmArgs(
         "-Xshare:off",
@@ -121,7 +95,7 @@ tasks.test {
     )
 }
 
-tasks.jar {
+tasks.named<Jar>("jar") {
     manifest {
         attributes(
             "Main-Class" to "no.nav.amt.distribusjon.ApplicationKt",
