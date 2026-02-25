ae7ef16329ecf9e688b68e559bf5e36fe9e9f165
robertkittilsen <robert.graarup.kittilsen@nav.no>
2026-02-25 14:54:57 +0100

    Endrer slik at alle endringer som blir gjort ikke blir journalført før det har gått en halvtime siden siste endring blir

--- src/main/kotlin/no/nav/amt/distribusjon/journalforing/job/EndringsvedtakJob.kt
+++ src/main/kotlin/no/nav/amt/distribusjon/journalforing/job/EndringsvedtakJob.kt
@@ -19,20 +19,31 @@ class EndringsvedtakJob(
     fun startJob() = jobManager.startJob(
         name = this.javaClass.simpleName,
         initialDelay = Duration.of(5, ChronoUnit.MINUTES),
-        period = Duration.of(10, ChronoUnit.MINUTES),
+        period = Duration.of(30, ChronoUnit.MINUTES),
     ) {
+        val enHalvtimeSiden = LocalDateTime.now().minusMinutes(30)
         val endringsvedtak = hendelseRepository
-            .getIkkeJournalforteHendelser(LocalDateTime.now().minusMinutes(30))
+            .getIkkeJournalforteHendelser(enHalvtimeSiden)
             .filter { it.hendelse.erEndringsVedtakSomSkalJournalfores() }
 
         val endringsvedtakPrDeltaker = endringsvedtak.groupBy { it.hendelse.deltaker.id }
 
-        endringsvedtakPrDeltaker.forEach { entry ->
-            log.info("Behandler endringsvedtak for deltaker med id ${entry.key}")
-            try {
-                journalforingService.journalforOgDistribuerEndringsvedtak(entry.value)
-            } catch (e: Exception) {
-                log.error("Behandling av endringsvedtak for deltaker med id ${entry.key} feilet", e)
+        endringsvedtakPrDeltaker.forEach { (deltakerId, hendelser) ->
+            /*
+             * Prosesser kun hendelser for en deltaker hvis den nyeste hendelsen er eldre enn 30 minutter,
+             *  for å samle alle endringer gjort i løpet av en halvtime i samme brev.
+             * Dette for å unngå å sende flere endringsvedtak etter hverandre hvis det gjøres flere endringer på samme deltaker i løpet av kort tid.
+             */
+            val nyesteHendelseOpprettet = hendelser.maxByOrNull { it.hendelse.opprettet } ?: return@forEach
+            if (nyesteHendelseOpprettet.hendelse.opprettet.isBefore(enHalvtimeSiden)) {
+                log.info("Behandler ${hendelser.size} endringsvedtak for deltaker med id $deltakerId")
+                try {
+                    journalforingService.journalforOgDistribuerEndringsvedtak(hendelser)
+                } catch (e: Exception) {
+                    log.error("Behandling av endringsvedtak for deltaker med id $deltakerId feilet", e)
+                }
+            } else {
+                log.info("Venter med å behandle endringsvedtak for deltaker $deltakerId (nyeste hendelse: $nyesteHendelseOpprettet)")
             }
         }
         log.info("Ferdig med å behandle ${endringsvedtak.size} endringsvedtak")
